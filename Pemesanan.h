b68dbb0e577bb2cf956bd3f4f91057399d392b4b
2503010110-source <2503010110@unper.ac.id>
2026-06-22 20:20:11 +0700

    Update Pemesanan.h

--- Pemesanan.h
+++ Pemesanan.h
@@ -1,1 +1,26 @@
 
+#ifndef TIKETKONSER_H
+#define TIKETKONSER_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class TiketKonser {
+private:
+    string namaPemesan;
+    string kategori;
+    int jumlahTiket;
+    int hargaTiket;
+    int totalHarga;
+
+public:
+    TiketKonser();
+
+    void inputData();
+    void hitungTotal();
+    void tampilData();
+};
+
+#endif
