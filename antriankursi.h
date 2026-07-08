f78e1d9592bfb2b8457972718fc5aa07cbef5eb1
2503010114-AhmadFajarHabibi <2503010114@unper.ac.id>
2026-07-08 09:03:50 +0700

    Refactor header guard and include statements

--- antriankursi.h
+++ antriankursi.h
@@ -1,33 +1,9 @@
-#ifndef SEATQUEUE_H
-#define SEATQUEUE_H
+#ifndef ANTRIANKURSI_H
+#define ANTRIANKURSI_H
 
-#include <iostream>
-#include <queue>
-#include <string>
+#include "Pemesanan.h"
 
-using namespace std;
-
-class SeatQueue {
-private:
-    queue<string> antreanPembeli;
-    int totalKursi;
-    int kursiTersedia;
-
-public:
-    // Constructor
-    SeatQueue(int kapasitas);
-
-    // Queue Pembelian
-    void tambahAntrean(string nama);
-    void layaniPembeli();
-    void tampilAntrean();
-
-    // Manajemen Kursi
-    bool pesanKursi(int jumlah);
-    void tampilKapasitas();
-
-    // Getter
-    int getKursiTersedia();
-};
+void prosesAntreanPembelian();
+void cekKapasitasKursi();
 
 #endif
